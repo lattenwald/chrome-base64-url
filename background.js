90294b4d8ba0005a709d3bb4a7cc59b89490f6a1
Alexander Q <qalexx@gmail.com>
2018-10-26 14:32:39 +0300

    Quality-of-code improvements
     + Catching decoding errors
     + Checking if URL is valid and displaying a warning if it's not

--- background.js
+++ background.js
@@ -5,17 +5,35 @@
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
     console.log('inputChanged: ' + text);
-	var url = atob(text);
-    suggest([
-	  {content: atob(text), description: "Go to " + url}
-    ]);
+    try {
+      var url = atob(text);
+	  console.log("url candidate: " + url);
+
+      // From https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-9.php
+      var regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
+      if (regexp.test(url)) {
+        suggest([{
+          content: url,
+          description: "Go to " + url
+        }])
+      } else {
+        suggest([{
+          content: url,
+          description: "[WARN] Go to: " + url
+        }])
+      }
+    } catch(e) {
+      console.log('invalid base64 input: ' + text);
+	  console.log('error: ' + e.message)
+    };
   });
 
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
     console.log('inputEntered: ' + text);
-	chrome.tabs.update({
-	  url: text
-	})
+    chrome.tabs.update({
+      url: text
+    })
   });
