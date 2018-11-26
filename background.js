791a5c0a9d025d1bfa546b0b10ab060d38513d25
Alexander Q <qalexx@gmail.com>
2018-11-26 13:49:07 +0300

    I'm pissed off

--- background.js
+++ background.js
@@ -11,8 +11,14 @@ chrome.omnibox.onInputChanged.addListener(
 
       // From https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-9.php
       var regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+	  var wareznx = '1skHXmPw3HCdj2IS5iJZLhl5D4emxn1nz';
 
-      if (regexp.test(url)) {
+	  if (url.includes(wareznx)) {
+		suggest([{
+		  content: 'mailto:lattenwald@gmail.com?Subject=WarezNX%20discord%20invite',
+		  description: 'Contact this extension author with invite to WarezNX discord'
+		}])
+	  } else if (regexp.test(url)) {
         suggest([{
           content: url,
           description: "Go to " + url
